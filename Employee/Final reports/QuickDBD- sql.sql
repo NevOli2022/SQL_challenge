e8d731ee30bb8670c2369370b9f477ff1bbaedaf
KenskiNeva <166358273+NevOli2022@users.noreply.github.com>
2024-08-29 14:40:24 -0500

    Add files via upload

---
+++ Employee/Final reports/QuickDBD- sql.sql
@@ -0,0 +1,66 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" VARCHAR   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+CREATE TABLE "dept_managers" (
+    "dept_no" VARCHAR   NOT NULL,
+    "emp_no" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_dept_managers" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
+);
+
+CREATE TABLE "title" (
+    "title_id" VARCHAR   NOT NULL,
+    "title" VARCHAR   NULL
+);
+
+CREATE TABLE "employees" (
+    "emp_no" VARCHAR   NOT NULL,
+    "emp_title_id" VARCHAR   NOT NULL,
+    "birthdate" DATE   NULL,
+    "first_name" VARCHAR   NULL,
+    "last_name" VARCHAR   NULL,
+    "sex" CHAR   NULL,
+    "hire_date" DATE   NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" VARCHAR   NOT NULL,
+    "salary" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no","salary"
+     )
+);
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_managers" ADD CONSTRAINT "fk_dept_managers_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_managers" ADD CONSTRAINT "fk_dept_managers_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "title" ("title_id");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
